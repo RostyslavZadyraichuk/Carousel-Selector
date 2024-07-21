df854e401550ed975025f11fba379b08f7ec7959
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2024-07-21 13:46:32 +0300

    Small fixes; migrate javafx dependencies into maven

--- src/main/java/com/zadyraichuk/selector/SelectorApp.java
+++ src/main/java/com/zadyraichuk/selector/SelectorApp.java
@@ -1,17 +1,18 @@
 package com.zadyraichuk.selector;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
 import com.zadyraichuk.general.PropertiesFile;
 import com.zadyraichuk.selector.controller.SelectorUIController;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
 
 public class SelectorApp extends Application {
 
@@ -23,30 +24,16 @@ public class SelectorApp extends Application {
         PROPERTIES = loadAppProperties();
     }
 
-    private static PropertiesFile loadAppProperties() {
-        URL path = SelectorApp.class.getResource("../../../selector/app.properties");
-        File propertiesFile = new File(Objects.requireNonNull(path).getPath());
-        return new PropertiesFile(propertiesFile);
+    public static void main(String[] args) {
+        launch(args);
     }
-
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
 //        Thread.setDefaultUncaughtExceptionHandler(this::catchException);
 
         controller = setUpPrimaryStage(primaryStage);
         controller.init();
-    }
-
-    private SelectorUIController setUpPrimaryStage(Stage primaryStage) throws IOException {
-        FXMLLoader loader =
-                new FXMLLoader(Objects.requireNonNull(getClass().getResource("../../../selector/ui/xml/selector.fxml")));
-        Parent root = loader.load();
-        primaryStage.setTitle("Wheel Selector");
-        primaryStage.setScene(new Scene(root, 550, 650));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-
-        return loader.getController();
     }
 
     @Override
@@ -61,4 +48,21 @@ public class SelectorApp extends Application {
 ////        e.printStackTrace();
 //    }
 
+    private static PropertiesFile loadAppProperties() {
+        URL path = SelectorApp.class.getResource("../../../selector/app.properties");
+        File propertiesFile = new File(Objects.requireNonNull(path).getPath());
+        return new PropertiesFile(propertiesFile);
+    }
+
+    private SelectorUIController setUpPrimaryStage(Stage primaryStage) throws IOException {
+        FXMLLoader loader =
+                new FXMLLoader(Objects.requireNonNull(getClass().getResource("../../../selector/ui/xml/selector.fxml")));
+        Parent root = loader.load();
+        primaryStage.setTitle("Wheel Selector");
+        primaryStage.setScene(new Scene(root, 550, 650));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        return loader.getController();
+    }
 }
